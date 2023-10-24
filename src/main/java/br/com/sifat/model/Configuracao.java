ff3ffd16036d014f35daf5de763ad521c186fa57
Gilberto Junior <ti@sifat.com.br>
2023-10-24 16:35:43 +0000

    Update Configuracao.java

--- src/main/java/br/com/sifat/model/Configuracao.java
+++ src/main/java/br/com/sifat/model/Configuracao.java
@@ -1,7 +1,7 @@
 package br.com.sifat.model;
 
 
-import takarta.persistence.*;
+import aakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
