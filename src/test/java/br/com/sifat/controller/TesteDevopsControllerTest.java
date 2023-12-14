5a43f7e72efed8f20d6e686e9edd20d3097e92fa
Rafael Massoni <desenvolvimento02@sifat.com.br>
2023-12-14 15:27:12 -0300

    Implementando jacoco

---
+++ src/test/java/br/com/sifat/controller/TesteDevopsControllerTest.java
@@ -0,0 +1,35 @@
+package br.com.sifat.controller;
+
+import br.com.sifat.service.ConfiguracaoService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class TesteDevopsControllerTest {
+
+    @InjectMocks
+    private TesteDevopsController testeDevopsController;
+
+    @Mock
+    private ConfiguracaoService configuracaoService;
+
+    @Test
+    void testeApiEmFuncionamento() {
+
+        testeDevopsController.testeApiEmFuncionamento();
+    }
+
+    @Test
+    void testeApiEmFuncionamentoComBanco() {
+    }
+
+    @Test
+    void buscarPorId() {
+    }
+}
